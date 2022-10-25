128a4d2309f0c97ee3982cb3a8a8a44b73ce35ad
Ole Kristian Sjøbakken <ole.kristian.sjobakken@bufdir.no>
2022-10-25 18:45:52 +0200

    Initial Commit of Blazor POC

---
+++ Birk.Client.Bestilling/Common/Person.cs
@@ -0,0 +1,17 @@
+﻿namespace Birk.Client.Bestilling.Common
+{
+    public static class Barn
+    {
+        public static int Personnummer { get; set; }
+        public static DateTime? Født { get; set; }
+        public static string Fornavn { get; set; }
+        public static string Etternavn { get; set; }
+        public static string BiRKID { get; set; }
+
+        public static int Alder 
+        { 
+            get; 
+            set; 
+        }
+    }
+}
