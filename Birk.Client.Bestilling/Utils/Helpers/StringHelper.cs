9ef6f6271b2321116be2e0ab513fc3aaa6b354c9
Cyril Besnard <cyril.besnard@soprasteria.com>
2023-04-14 01:36:29 +0200

    transition + new css class + get rid of stringhelper

--- Birk.Client.Bestilling/Utils/Helpers/StringHelper.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Birk.Client.Bestilling.Utils.Helpers
-{
-    public static class StringHelper
-    {
-        public static string BuildPlaceholder(string input) { return $"Skriv inn {input}"; }
-    }
-}
