fb28a5ea65361c41c26c28828dac5766bb1f1046
Cyril Besnard <cyril.besnard@soprasteria.com>
2023-04-17 00:40:15 +0200

    css button active hover colros + stringhelper for top-panel

---
+++ Birk.Client.Bestilling/Utils/Helpers/StringHelper.cs
@@ -0,0 +1,59 @@
+﻿namespace Birk.Client.Bestilling.Utils.Helpers
+{
+    public static class StringHelper
+    {
+        public static string ConvertFødt(DateTime? født)
+        {
+            if (født == null) return "";
+
+            return $"{født.Value.Day}/{født.Value.Month}/{født.Value.Year}";
+        }
+
+        public static string CalculateAge(DateTime birthdate)
+        {
+            DateTime today = DateTime.Today;
+            int ageInYears = today.Year - birthdate.Year;
+            int months = today.Month - birthdate.Month;
+            if (birthdate > today.AddYears(-ageInYears))
+            {
+                ageInYears--;
+                months = 12 - birthdate.Month + today.Month;
+            }
+
+            if (months == 12)
+            {
+                ageInYears++;
+                months = 0;
+            }
+
+            string age = $"{ageInYears} år{(ageInYears != 1 ? "" : "e")}";
+
+            if (months > 0)
+            {
+                age += $" og {months} måned{(months != 1 ? "er" : "")}";
+            }
+
+            return age;
+        }
+
+        public static string ConvertGender(int numericGender)
+        {
+            string gender = "";
+            switch (numericGender)
+            {
+                case 1:
+                    gender = "Gutt";
+                    break;
+                case 2:
+                    gender = "Jente";
+                    break;
+                case 3:
+                    gender = "Ukjent";
+                    break;
+                default:
+                    break;
+            }
+            return gender;
+        }
+    }
+}
